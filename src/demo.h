3c3f4b667c6fef220a948cf882e9b45772eeff88
Hiro-gamer-JP <72668989+Hiro-gamer@users.noreply.github.com>
2020-10-11 07:22:10 +0900

    機能の追加
    /*** 書き加えたファイル ***/
    /* include */
    darknet.h
    /* src */
    coco.c
    darknet.c
    demo.c
    demo.h
    detector.c
    image.c
    image.h
    image_opencv.cpp
    image_opencv.h
    util.c
    yolo.c
    
    
    /*** 追加したオプション ***/
    /* 画像のみ(test only) */
    -print_detection_layer //detection_layerの表示を行う
    /* 動画と画像(test & demo) */
    -draw_label //画像や動画にラベルを出力
    -print_coordinate //標準出力を行う(座標や認識率の出力)
    -no_total //合計値を出力
    -print_warning //警告の表示
    /* 動画のみ(demo only) */
    -mosaic //モザイクを入れる->0(無),1(有),2(bboxを描かない[フレーム取得用])
    -show_frame //フレーム番号を出力
    -print_fps //fpsを表示する

--- src/demo.h
+++ src/demo.h
@@ -1,12 +1,13 @@
 #ifndef DEMO_H
-#define DEMO_H
+#define DEMO_Hlabel_flag
 
 #include "image.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
+//ˆø”‚ğ‘‚â‚µ‚Ä‚¢‚é
 void demo(char *cfgfile, char *weightfile, float thresh, float hier_thresh, int cam_index, const char *filename, char **names, int classes, int avgframes,
-    int frame_skip, char *prefix, char *out_filename, int mjpeg_port, int dontdraw_bbox, int json_port, int dont_show, int ext_output, int letter_box_in, int time_limit_sec, char *http_post_host, int benchmark, int benchmark_layers);
+    int frame_skip, char *prefix, char *out_filename, int mjpeg_port, int dontdraw_bbox, int json_port, int dont_show, int ext_output, int letter_box_in, int time_limit_sec, char *http_post_host, int benchmark, int benchmark_layers, int mosaic_flag, int draw_label, int print_coordinate, int no_total, int show_frame_num, int print_fps, int print_warning);
 #ifdef __cplusplus
 }
 #endif
